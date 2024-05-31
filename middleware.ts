5bec2007e35d6c8fd94c7ace67996514a40afd98
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-05-31 09:00:32 +0200

    fix: Refactor middleware.ts and update authentication handling

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,16 @@
-export { authHandler as middleware } from "@/auth";
+import { authHandler } from "@/auth";
+import { NextResponse } from "next/server";
+
+export async function middleware(request: Request) {
+  const session = await authHandler();
+
+  if (session && session.user) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/login"],
+};
