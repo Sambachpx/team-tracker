58eebf5657a7e178a534277f32e1617e39a6b339
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-05-31 10:44:20 +0200

    fix: remove bcrypt utility function

--- utils/bcrypt.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-
-export async function saltAndHashPassword(password: string) {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  return hashedPassword;
-}
